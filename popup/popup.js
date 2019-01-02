b412cfc5a2d0cb07cc9fa1e4d0bd304002940b1b
Haixiang6123 <haixiang6123@gmail.com>
2019-01-01 23:09:23 -0800

    Update: popup pages, styles and scripts

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,3 @@
-let changeColor = document.getElementById('changeColor');
-
 // Change color of button
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
@@ -7,11 +5,25 @@ chrome.storage.sync.get('color', function(data) {
 });
 
 // Change document color
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'console.log("ok")'});
-    });
-};
+// changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id,
+//             {code: 'console.log("ok")'});
+//     });
+// };
+
+function initOptions() {
+    let themeSwitchEl = document.querySelector('#themeSwitch')
+    // From themes.js
+    themes.forEach((theme) => {
+        let optionEl = document.createElement('option')
+        optionEl.setAttribute('value', `${theme}.css`)
+        optionEl.innerText = theme
+
+        themeSwitchEl.appendChild(optionEl)
+    })
+}
+
+initOptions()
