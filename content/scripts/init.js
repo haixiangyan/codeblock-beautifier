40cc8d635c86b5881ea0047bae60eed71cf3ae4b
Haixiang6123 <haixiang6123@gmail.com>
2019-01-01 22:19:40 -0800

    Update: update theme with rebuilding <code/>

--- content/scripts/init.js
+++ content/scripts/init.js
@@ -2,6 +2,7 @@ class SelectionPanel {
     constructor(preEl, codeEl) {
         this.preEl = preEl
         this.codeEl = codeEl
+        this.lang = this.codeEl.classList[1]
         this.panel = this.generatePanelEl()
         this.langSelector = this.generateLangSelector()
 
@@ -20,14 +21,12 @@ class SelectionPanel {
 
     generateLangSelector() {
         let langSelectEl = document.createElement('select')
-        let selections = langs
 
-        let lang = this.codeEl.classList[1]
-
-        selections.forEach((selection) => {
+        // From langs.js, building options for <select/>
+        langs.forEach((selection) => {
             let optionEl = document.createElement('option')
             optionEl.setAttribute('value', selection.value)
-            optionEl.selected = (lang === selection.value)
+            optionEl.selected = (this.lang === selection.value)
             optionEl.innerText = selection.text
 
             langSelectEl.appendChild(optionEl)
@@ -38,7 +37,11 @@ class SelectionPanel {
 
     bindEvents() {
         this.langSelector.addEventListener('change', (event) => {
+            // Update highlight language
             this.codeEl.setAttribute('class', `hljs ${event.target.value}`)
+            // Rewrite codes in <code/>
+            this.codeEl.innerText = this.codeEl.innerText
+            // Highlight the new code element
             hljs.highlightBlock(this.codeEl)
         })
     }
@@ -62,8 +65,8 @@ class CodeBlock {
 
         hljs.highlightBlock(this.codeEl)
 
+        // Init selection panel
         this.selectionPanel = new SelectionPanel(this.preEl, this.codeEl)
-
         this.wrapPanelEl()
     }
 
@@ -86,14 +89,8 @@ class CodeBlock {
     }
 }
 
-function highlight() {
-    hljs.initHighlighting()
-}
-
 let preEls = Array.from(document.querySelectorAll('pre'))
 
 let codeBlocks = preEls.map((preEl) => {
     return new CodeBlock(preEl)
 })
-
-// highlight()
