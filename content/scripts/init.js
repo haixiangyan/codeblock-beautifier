727a6bc18cd2ca14fe6725b4e386790c7226bcc3
Haixiang6123 <haixiang6123@gmail.com>
2019-01-01 19:10:05 -0800

    Update: add lang selector to code blocks

--- content/scripts/init.js
+++ content/scripts/init.js
@@ -1,12 +1,57 @@
+class SelectionPanel {
+    constructor() {
+        this.view = null
+
+        this.init()
+    }
+
+    init() {
+        let panelEl= document.createElement('div')
+
+        let langSelectEl = this.generateLangSelector()
+
+        panelEl.appendChild(langSelectEl)
+
+        this.view = panelEl
+    }
+
+    generateLangSelector() {
+        let langSelectEl = document.createElement('select')
+        let selections = [
+            {value: 'javascript', text: 'JavaScript'},
+            {value: 'java', text: 'Java'},
+            {value: 'css', text: 'CSS'},
+            {value: 'python', text: 'Python'},
+        ]
+
+        selections.forEach((selection) => {
+            let optionEl = document.createElement('option')
+            optionEl.setAttribute('value', selection.value)
+            optionEl.innerText = selection.text
+
+            langSelectEl.appendChild(optionEl)
+        })
+
+        return langSelectEl
+    }
+}
+
 class CodeBlock {
     constructor() {
         this.codes = Array.from(document.querySelectorAll('pre'))
+
+        this.init()
     }
+
     init() {
         this.codes.forEach((preEl) => {
             let codeEl = this.generateCodeEl(preEl)
 
+            let selectionPanel = new SelectionPanel()
+
             this.wrapCodeEl(preEl, codeEl)
+
+            preEl.appendChild(selectionPanel.view)
         })
     }
 
@@ -25,11 +70,12 @@ class CodeBlock {
     }
 }
 
+
+
 function highlight() {
     hljs.initHighlighting()
 }
 
 let codeBlock = new CodeBlock()
-codeBlock.init()
 
 highlight()
